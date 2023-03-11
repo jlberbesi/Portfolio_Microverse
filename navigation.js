c41f9d4bd8be97034b567fed34fb7938e2f382c4
imberbesi <jlberbessi@gmail.com>
2023-03-11 10:21:39 -0500

    Add objects file

--- navigation.js
+++ navigation.js
@@ -25,3 +25,38 @@ document.addEventListener("click", (event) => {
       document.body.classList.remove("no-scroll");
    }
 });
+
+
+// Get the popup element
+var popup = document.querySelector('.popup');
+
+// Get the button that triggers the popup
+var btn = document.querySelector('.popup-btn');
+
+// Get the close button
+var close = popup.querySelector('.close');
+
+// When the user clicks the button, display the popup
+btn.addEventListener('click', function() {
+  popup.style.display = 'block';
+});
+
+// When the user clicks the close button, hide the popup
+close.addEventListener('click', function() {
+  popup.style.display = 'none';
+});
+
+// No scrooll down . Get the body element
+var body = document.querySelector('body');
+
+// When the user clicks the button, display the popup and disable scrolling
+btn.addEventListener('click', function() {
+popup.style.display = 'block';
+body.style.overflow = 'hidden';
+});
+
+// When the user clicks the close button, hide the popup and re-enable scrolling
+close.addEventListener('click', function() {
+popup.style.display = 'none';
+body.style.overflow = 'auto';
+});
